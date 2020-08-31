4070f50877ff182b93f1586e0482d6f0ba851dc7
Philip McGrath <philip@philipmcgrath.com>
2020-08-31 16:00:14 -0400

    v0.3.3: drop Racket 6.12 (lacks #:track-literals); fix ci & pkg deps

--- info.rkt
+++ info.rkt
@@ -3,22 +3,22 @@
 (define pkg-name "adjutor")
 (define collection "adjutor")
 (define pkg-desc "A helper library")
-(define version "0.3.2")
+(define version "0.3.3")
 (define pkg-authors '(philip))
 
-;; Dependencies:
-(define deps '(("base" #:version "6.12")
-               "static-rename-lib"))
-
-(define build-deps '("scribble-lib"
-                     "racket-doc"
-                     "rackunit-lib"
-                     "rackunit-spec"
-                     "scribble-doc"))
-
 ;; Documentation:
-(define scribblings '(("scribblings/adjutor.scrbl"
-                       (multi-page)
-                       )))
+(define scribblings
+  '(("scribblings/adjutor.scrbl" (multi-page))))
 
+;; Dependencies:
+(define deps
+  '(["base" #:version "7.0"]
+    "static-rename-lib"))
+
+(define build-deps
+  '("scribble-lib"
+    "racket-doc"
+    "rackunit-lib"
+    "rackunit-spec"
+    "scribble-doc"))
 
